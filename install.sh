01d2021aed71f78f9e27454e978cce876fb58fe5
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-14 02:38:26 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+clear
+echo "Installing..."
+sleep 2
+chmod a+x smspeak.py
+sudo cp smspeak.py /usr/lib/python3.6
+echo ""
+echo "Uninstalling..., Finish...!"
+echo ""
+exit 1
